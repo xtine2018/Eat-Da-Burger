661ca4966919a06ab88bc254507db15b59223494
Jobi Mathai <40534877+jobimathai1@users.noreply.github.com>
2018-12-17 22:25:49 -0800

    updated connection.js
    Added your jawsDB connection details on lines 8-11

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,31 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-connection = mysql.createConnection({
-  port: 3306,
-  host: "127.0.0.1",
-  user: "root",
-  password: "j31blue",
-  database: "burgers_db"
-});
-};
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+	connection = mysql.createConnection({
+	host: 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'v6dymq77y28v36ow',
+	password:'mylbedz39vhz95jw',
+	database: 'sbrznbfxqz2hitcq'
+	})
+}
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id: " + connection.threadId);
+// var connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'password',
+// 	database: 'burgers_db'
+// });
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('Connection Sucessful! and connected as id ' + connection.threadId);
 });
 
-// connection.connect();
 module.exports = connection;
